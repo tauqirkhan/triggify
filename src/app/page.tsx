43adb350ab9b1c0806343831c3279bc5c038a31b
tauqirkhan <tauqirkhann.11@gmail.com>
2025-11-11 21:38:34 +0530

    feat: setup

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      <Button variant="outline" className='text-2xl'>
+        Click Me
+      </Button>
+    </div>
+  )
+}
+
+export default page
