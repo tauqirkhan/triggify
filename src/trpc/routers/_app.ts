e9801051923f655238761a83d9f7b73e667d4395
tauqirkhan <tauqirkhann.11@gmail.com>
2025-11-24 23:14:05 +0530

    feat: add ai-providers

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,11 +1,22 @@
 import { inngest } from '@/inngest/client';
 import { createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
-import { email } from 'zod';
+import { google } from '@ai-sdk/google';
+import { generateText } from 'ai';
 
 
 // data access layer
 export const appRouter = createTRPCRouter({
+  testAi: protectedProcedure.mutation(async() => {
+    await inngest.send({
+      name: "execute/ai"
+    })
+
+    return {
+        success: true,
+        message: "Job Queued"
+    }
+  }),
   getWorkflows: protectedProcedure.query(async ({ctx}) => {
       return prisma.workflow.findMany()
     }),
