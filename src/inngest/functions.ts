9101bcb9623a8da33f84fa337c32070c3bdb9128
tauqirkhan <tauqirkhann.11@gmail.com>
2025-11-25 22:37:52 +0530

    feat: add error tracking with sentry

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -2,6 +2,7 @@ import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { inngest } from "./client";
 import { generateText } from "ai";
 import { createOpenAI } from "@ai-sdk/openai"
+import * as Sentry from "@sentry/nextjs";
 
 const google = createGoogleGenerativeAI()
 const openai = createOpenAI()
@@ -12,13 +13,21 @@ export const execute = inngest.createFunction(
   async ({ event, step }) => {
     await step.sleep("pretend", "5s")
 
+    // This will show logs on sentry 
+    Sentry.logger.info('User triggered test log', { log_source: 'sentry_test' })
+
     const {steps: geminiSteps} = await step.ai.wrap(
       "using-gemini-ai",
       generateText, 
       {
         model: google("gemini-2.5-flash"),
         system: "You are a helpful assistant",
-        prompt: "What is 2 / 10"
+        prompt: "What is 2 / 10",
+        experimental_telemetry: {
+        isEnabled: true,
+        recordInputs: true,
+        recordOutputs: true,
+        },
       }
     )
 
@@ -28,7 +37,12 @@ export const execute = inngest.createFunction(
       {
         model: openai("gpt-4.1-mini"),
         system: "You are a helpful assistant",
-        prompt: "What is 2 / 10"
+        prompt: "What is 2 / 10",
+        experimental_telemetry: {
+        isEnabled: true,
+        recordInputs: true,
+        recordOutputs: true,
+        },
       }
     )
     
